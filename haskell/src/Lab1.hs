ce38b48c492038447c2a9f0d4dc357d87e6ee1ad
viktorfrom <viktor.from91@gmail.com>
2020-04-08 14:58:33 +0200

    Add Lab1.hs

---
+++ haskell/src/Lab1.hs
@@ -0,0 +1,12 @@
+module Lab1
+    ( smallest_k_sets
+    ) where
+
+smallest_k_sets :: (Integral a) => [a]
+smallest_k_sets = result
+
+list :: (Integral a) => [a]
+list = [1,2,3]
+
+result :: (Integral a) => [a]
+result = list
