0f35771471e3ce8040f6e6c0b48930dee3e92f3e
viktorfrom <viktor.from91@gmail.com>
2020-04-15 13:44:14 +0200

    Update Lab1.hs

--- haskell/src/Lab1.hs
+++ haskell/src/Lab1.hs
@@ -3,22 +3,25 @@ module Lab1
     ) where
 
 smallest_k_sets :: [(Int, Int, [Int])]
-smallest_k_sets = generate_subsets list3 1
-  -- | length list3==0 = error("Invalid input: list is empty!")
-  -- | otherwise = generate_subsets list3 1
+smallest_k_sets = generate_subsets2 (check_length list3) 1
+
 
 -- Generate subsets right to left
 generate_subsets1 :: [Int] -> Int -> [(Int, Int, [Int])]
 generate_subsets1 xs i
   | length xs==0 = []
-  | otherwise = (sum xs, i, xs) : generate_subsets1 (init xs) i
+  | otherwise = (sum xs, i, xs) : generate_subsets1 (init xs) (i + 1)
 
 -- Generate subsets left to right
-generate_subsets :: [Int] -> Int -> [(Int, Int, [Int])]
-generate_subsets xs i
+generate_subsets2 :: [Int] -> Int -> [(Int, Int, [Int])]
+generate_subsets2 xs i
   | length xs==0 = []
-  | otherwise = (sum xs, i, xs) : generate_subsets (tail xs) (i + 1)
+  | otherwise = generate_subsets1 xs i ++ generate_subsets2 (tail xs) (i + 1)
 
+check_length :: [Int] -> [Int]
+check_length xs
+  | length xs==0 = error("Invalid input: list is empty!")
+  | otherwise = xs
 
 quick_sort :: Ord a => [a] -> [a]
 quick_sort [] = []
