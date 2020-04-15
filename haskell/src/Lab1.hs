5b6e8386f431fbd638c88baeb7ce818332bef685
viktorfrom <viktor.from91@gmail.com>
2020-04-15 15:06:25 +0200

    Update Lab1.hs

--- haskell/src/Lab1.hs
+++ haskell/src/Lab1.hs
@@ -2,27 +2,27 @@ module Lab1
     ( smallest_k_sets
     ) where
 
-smallest_k_sets :: [(Int, Int, [Int])]
-smallest_k_sets = generate_subsets (check_length list3) 1
+smallest_k_sets :: [(Int, Int, Int, [Int])]
+smallest_k_sets = generate_subsets (check_length list3) 1 (length list3)
 
 -- Generate subsets subroutine, left to right
-subroutine_subsets :: [Int] -> Int -> [(Int, Int, [Int])]
-subroutine_subsets xs i
+subroutine_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
+subroutine_subsets xs i j
   | length xs==0 = []
-  | otherwise = (sum xs, i, xs) : subroutine_subsets (init xs) (i + 1)
+  | otherwise = (sum xs, i, j, xs) : subroutine_subsets (init xs) i (j - 1)
 
 -- Generate subsets
-generate_subsets :: [Int] -> Int -> [(Int, Int, [Int])]
-generate_subsets xs i
+generate_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
+generate_subsets xs i j
   | length xs==0 = []
-  | otherwise = subroutine_subsets xs i ++ generate_subsets (tail xs) (i + 1)
+  | otherwise = subroutine_subsets xs i j ++ generate_subsets (tail xs) (i + 1) j
 
 check_length :: [Int] -> [Int]
 check_length xs
   | length xs==0 = error("Invalid input: list is empty!")
   | otherwise = xs
 
-quick_sort :: [(Int, Int, [Int])] -> [(Int, Int, [Int])]
+quick_sort :: [(Int, Int, Int, [Int])] ->  [(Int, Int, Int, [Int])]
 quick_sort [] = []
 quick_sort (x : xs) = quick_sort less ++ equal ++ quick_sort larger
   where
