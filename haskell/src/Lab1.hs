2d1bcc6782d40f02be33ef8a0dc554cfec429487
viktorfrom <viktor.from91@gmail.com>
2020-04-16 09:34:46 +0200

    Update Lab1.hs

--- haskell/src/Lab1.hs
+++ haskell/src/Lab1.hs
@@ -2,8 +2,7 @@ module Lab1
     ( smallest_k_sets
     ) where
 
-smallest_k_sets :: [(Int, Int, Int, [Int])]
-smallest_k_sets = generate_subsets (check_length list3) 1 (length list3)
+
 
 -- Generate subsets subroutine, left to right
 subroutine_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
@@ -17,25 +16,31 @@ generate_subsets xs i j
   | length xs==0 = []
   | otherwise = subroutine_subsets xs i j ++ generate_subsets (tail xs) (i + 1) j
 
+quick_sort :: [(Int, Int, Int, [Int])] -> [(Int, Int, Int, [Int])]
+quick_sort [] = []
+quick_sort (pivot : xs) = (quick_sort lesser) ++ [pivot] ++ (quick_sort greater)
+    where
+        lesser  = filter (< pivot) xs
+        greater = filter (>= pivot) xs
+
 check_length :: [Int] -> [Int]
 check_length xs
   | length xs==0 = error("Invalid input: list is empty!")
   | otherwise = xs
 
-quick_sort :: [(Int, Int, Int, [Int])] ->  [(Int, Int, Int, [Int])]
-quick_sort [] = []
-quick_sort (x : xs) = quick_sort less ++ equal ++ quick_sort larger
-  where
-    less   = [y | y<-xs, y<x ]
-    equal  = [y | y<-xs, y==x] ++ [x]
-    larger = [y | y<-xs, y>x ]
+result :: [Int] -> Int -> [(Int, Int, Int, [Int])]
+result xs k
+  | k <= 0 = error("Invalid input: second parameter needs to be >= 0!")
+  | otherwise = take k (quick_sort(generate_subsets (check_length xs) 1 (length xs)))
 
+smallest_k_sets :: [(Int, Int, Int, [Int])]
+smallest_k_sets = result list3 3
 
-list3 :: [Int] 
+list3 :: [Int]
 list3 = [-1, 2, -3, 4, -5]
 
-list2 :: [Int] 
-list2 = [-1]
+-- list2 :: [Int] 
+-- list2 = [-1]
 
-list1 :: [Int] 
-list1 = []
+-- list1 :: [Int] 
+-- list1 = []
