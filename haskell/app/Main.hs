b96de64df2a8591d1700c25fdb550c91bd456e2a
viktorfrom <viktor.from91@gmail.com>
2020-04-22 10:59:00 +0200

    Add Lab3.hs

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,10 +2,14 @@ module Main where
 
 import Lab1
 import Lab2
+import Lab3
 
 main :: IO () 
 -- Lab1
---main = putStr smallest_k_sets 
+-- main = putStr smallest_k_sets 
 
 -- Lab2
 main = print result
+
+-- Lab3
+-- main = print result
