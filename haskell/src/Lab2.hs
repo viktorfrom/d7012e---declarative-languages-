a75dd564a757278b5c09e2c5dbdf0beea8ffdcba
viktorfrom <viktor.from91@gmail.com>
2020-04-23 10:38:05 +0200

    Update Lab2.hs

--- haskell/src/Lab2.hs
+++ haskell/src/Lab2.hs
@@ -145,7 +145,6 @@ newton_raphson fn fn_prim x0
   | otherwise = newton_raphson fn fn_prim x1
   where
     x1 = x0 - (fn x0) / (fn_prim x0)
-    --x1_next = x1 - (fn x1) / (fn_prim x1)
 
 -- findzero ::  String -> String -> Float -> Float
 -- findzero s1 s2 x0 
