bdb083d30a0390824e1f2f058f85308ce6b3f83a
viktorfrom <viktor.from91@gmail.com>
2020-04-28 13:34:49 +0200

    Update Statement.hs

--- haskell/src/Statement.hs
+++ haskell/src/Statement.hs
@@ -16,25 +16,30 @@ data Statement =
     Write Expr.T
     deriving Show
 
+-- Remove ":=", ";" and keep word/var. Assign val to word/var.
 assignment = word #- accept ":=" # Expr.parse #- require ";" >-> buildAss
 buildAss (v, e) = Assignment v e
 
--- Remove "if", "then", "else". Save (expr) # (stmt) # (stmt) within if-stmt
-if_stmt = (accept "if" -# Expr.parse #- require "then") # (parse #- require "else") # parse >-> buildIf
+-- Remove "if", "then", "else". Save/parse "(expr) # (stmt) # (stmt)" within if_stmt.
+if_stmt = (accept "if" -# Expr.parse #- require "then") # 
+          (parse #- require "else") # parse >-> buildIf
 buildIf ((expr, stmt1), stmt2) = If expr stmt1 stmt2
 
-
+-- Remove "skip". ";" and do nothing.
 skip = accept "skip" #- require ";" >-> buildSkip
 buildSkip _ = Skip
 
+-- Remove "begin" and iterate through the list.
 begin = accept "begin" -# iter parse #- require "end" >-> Begin
 
--- Remove "while" and "do". Save/parse expr and stmt.
+-- Remove "while" and "do". Save/parse expr and stmt withing "while".
 while = (accept "while" -# Expr.parse #- require "do") # parse >-> buildWhile
 buildWhile (expr, stmt) = While expr stmt
 
-
+-- Remove "read", ";" and save the word/var
 read = accept "read" -# word #- require ";" >-> Read
+
+-- Remove "write", ";" and write expr to word/var
 write = accept "write" -# Expr.parse #- require ";" >-> Write
 
 exec :: [T] -> Dictionary.T String Integer -> [Integer] -> [Integer]
@@ -44,5 +49,5 @@ exec (If cond thenStmts elseStmts: stmts) dict input =
     else exec (elseStmts: stmts) dict input
 
 instance Parse Statement where
-  parse = error "Statement.parse not implemented"
+  parse = assignment ! if_stmt ! skip ! begin ! while ! Statement.read ! Statement.write
   toString = error "Statement.toString not implemented"
