a47505c02b16506838a4c438de31b9fe98863a17
viktorfrom <viktor.from91@gmail.com>
2020-04-29 18:11:05 +0200

    Update Program.hs

--- haskell/src/Program.hs
+++ haskell/src/Program.hs
@@ -3,9 +3,15 @@ import Parser hiding (T)
 import qualified Statement
 import qualified Dictionary
 import Prelude hiding (return, fail)
-newtype T = Program ([Statement.T]) -- Import type T, 7 constructors
+newtype T = Program ([Statement.T]) -- Import type T (7 constructors)
 instance Parse T where
-  parse = error "Program.parse not implemented"
-  toString = error "Program.toString not implemented"
-             
-exec = error "Program.exec not implemented"
+  -- Iterate through the program stmts
+  parse = (iter Statement.parse) >-> Program
+  toString = toStringFunc
+
+-- Convert program stmts into strings
+toStringFunc :: T -> String
+toStringFunc (Program (x:xs)) = Statement.toString x ++ toString (Program xs)
+
+-- Takes a program containing stmt xs, int xs and evaluates the input
+exec (Program xs) int_xs = Statement.exec xs Dictionary.empty int_xs
