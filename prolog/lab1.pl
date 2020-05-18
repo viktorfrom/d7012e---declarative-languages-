abe7609f08802befccbf35a0de7284f52c97ab5c
viktorfrom <viktor.from91@gmail.com>
2020-05-18 13:37:54 +0200

    Update lab1.pl

--- prolog/lab1.pl
+++ prolog/lab1.pl
@@ -7,37 +7,65 @@
 % A three-place relation:
 % move(state1, Move, state2)
 % State1 -> Move -> State2
-   
+    
 
-% Grab steel key, move to room 2.
+
+
+% Move from room 1 to room 2.
 move(state(r1, inventory, BrassKey, Package),
      walk(r1, r2),
      state(r2, inventory, BrassKey, Package)). 
 
-% Grab brass key, move to room 1.
-move(state(r2, inventory, inventory, Package),
+% Move from room 2 to room 1.
+move(state(r2, inventory, BrassKey, Package),
      walk(r2, r1),
-     state(r1, inventory, inventory, Package)). 
+     state(r1, inventory, BrassKey, Package)). 
 
-% Drop steel key, move to room 3.
+% Move from room 1 to room 3.
 move(state(r1, SteelKey, inventory, Package),
      walk(r1, r3),
      state(r3, SteelKey, inventory, Package)). 
 
-% Grab package, move to room 1.
-move(state(r3, SteelKey, inventory, inventory),
+% Move from room 3 to room 1.
+move(state(r3, SteelKey, inventory, Package),
      walk(r3, r1),
-     state(r1, SteelKey, inventory, inventory)). 
+     state(r1, SteelKey, inventory, Package)). 
 
-% Grab package, move to room 1.
-move(state(r3, SteelKey, inventory, inventory),
-     walk(r3, r1),
-     state(r1, SteelKey, inventory, inventory)). 
 
-% Drop brass key, grab steel key, move to room 2.
-move(state(r1, inventory, BrassKey, inventory),
-     walk(r1, r2),
-     state(r2, inventory, BrassKey, inventory)). 
+
+% Grab steel key
+move(state(r1, inventory, BrassKey, Package),
+     grab = "Picked up steel key.",
+     state(r1, inventory, BrassKey, Package)).
+
+% Grab brass key
+move(state(r2, SteelKey, inventory, Package),
+     grab = "Picked up brass key.",
+     state(r2, SteelKey, inventory, Package)). 
+
+% Grab package.
+move(state(r3, SteelKey, BrassKey, inventory),
+     grab = "Picked up package.",
+     state(r3, SteelKey, BrassKey, inventory)). 
+
+
+
+
+
+% Drop steel key.
+move(state(_, inventory, BrassKey, Package),
+     drop,
+     state(_, inventory, BrassKey, Package)). 
+
+% Drop brass key.
+move(state(_, SteelKey, inventory, Package),
+     drop,
+     state(_, SteelKey, inventory, Package)). 
+
+% Drop package.
+move(state(_, SteelKey, BrassKey, inventory),
+     drop,
+     state(_, SteelKey, BrassKey, inventory)). 
 
 canget(state( _, _, _, has)).       
 
