b237694ccd6b6d0f157436982b31114ecf87810c
viktorfrom <viktor.from91@gmail.com>
2020-05-22 13:39:51 +0200

    Update lab2.pl

--- prolog/lab2.pl
+++ prolog/lab2.pl
@@ -1,17 +1,14 @@
-
-
 % -- Generate subsets subroutine, left to right
 % subroutine_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
 % subroutine_subsets xs i j
 %   | length xs==0 = []
 %   | otherwise = (sum xs, i, j, xs) : subroutine_subsets (init xs) i (j - 1)
 
-% -- Generate subsets
-% -- Generate subsets subroutine, left to right
-% subroutine_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
-% subroutine_subsets xs i j
-%   | length xs==0 = []
-%   | otherwise = (sum xs, i, j, xs) : subroutine_subsets (init xs) i (j - 1)
+% Subroutine_subsets
+subroutine_subsets(XS, I, J, L) :-
+    % sum_list(XS, Sum),
+    init(XS, Init),
+    subroutine_subsets(Init, I, (J-1), L).
 
 % -- Generate subsets
 % generate_subsets :: [Int] -> Int -> Int -> [(Int, Int, Int, [Int])]
@@ -19,25 +16,30 @@
 %   | length xs==0 = []
 %   | otherwise = subroutine_subsets xs i j ++ generate_subsets (tail xs) (i + 1) j
 
-
-subroutine_subsets([], 0).
-subroutine_subsets(XS, I, J, L) :-
-    sum_list(XS, Sum_XS),
-    L = (Sum_XS, I, J, XS).
-
 % Generate subsets
-generate_subsets([], 0).
-generate_subsets(XS, L) :-
-    head(XS, Res), 
-    tail(XS, Res2), 
-    append([Res], Res2, L).
+generate_subsets([], _, _, []).
+generate_subsets(XS, I, J, [_ | Res]) :-
+    tail(XS, Tail), 
+    C is I + 1,
+    % subroutine_subsets(XS, I, J, X),
+    generate_subsets(Tail, C, J, Res).
 
+% Return list without head element
+init([_], []).
+init([Head | Tail], [Head | I]):-
+    init(Tail, I).
+
+% Return sum of list
 sum_list([], 0).
 sum_list([Head|Tail], Sum) :-
    sum_list(Tail, Rest),
    Sum is Head + Rest.
 
-head([X|_], X).
+% result([], "Invalid input, list empty!").
+% result(XS, Res) :-
+%     length(XS, Len),
+%     generate_subsets(XS, 1, Len, Res).
 
+head([X|_], X).
 
 tail([_|X], X).
