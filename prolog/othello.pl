58e3e4cba7473ab9d9f3ef309a3008d6bdb986e9
viktorfrom <viktor.from91@gmail.com>
2020-06-04 21:40:14 +0200

    Update lab3.pl

--- prolog/othello.pl
+++ prolog/othello.pl
@@ -3,8 +3,8 @@
 %    D7012E Declarative languages
 %    Luleå University of Technology
 %
-%    Student full name: <TO BE FILLED IN BEFORE THE GRADING> 
-%    Student user id  : <TO BE FILLED IN BEFORE THE GRADING> 
+%    Student full name: Viktor From
+%    Student user id  : vikfro-6
 %
 /* ------------------------------------------------------- */
 
